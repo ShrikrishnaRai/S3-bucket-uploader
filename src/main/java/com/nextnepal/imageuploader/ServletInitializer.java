f17a511562d72226df5722c97ce801be7dd40e1c
Shree Rai <cri_rai@outlook.com>
2020-09-05 08:35:32 +0545

    Update ServletInitializer.java

--- src/main/java/com/nextnepal/imageuploader/ServletInitializer.java
+++ src/main/java/com/nextnepal/imageuploader/ServletInitializer.java
@@ -3,6 +3,7 @@ package com.nextnepal.imageuploader;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
+//This class is used to display swagger ui in the browser it avoids (cannot find swagger endpoint)
 public class ServletInitializer extends SpringBootServletInitializer {
 
     @Override
